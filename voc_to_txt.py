6c3619ad67371aaae6e1ff56b721b46b3f414f21
lzq <754802201@qq.com>
2021-02-04 23:33:44 +0800

    voc_to_txt.py

---
+++ voc_to_txt.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/2/4 23:03
+# @Author : lzq
+# @Site : 
+# @File : voc_to_txt.py
+# @Software: PyCharm Community Edition 
+import sys
+import os
+import glob
+import xml.etree.ElementTree as ET
+
+if __name__ == '__main__':
+    outfile = './test/aircraft_79.txt'
+    xml = "./test/aircraft_79.xml"
+    with open(outfile, "w") as new_f:
+        root = ET.parse(xml).getroot()
+        filename = root.find('filename').text
+        size = root.find('size')
+        width = size.find('width').text
+        height = size.find('height').text
+        new_f.write("%s %s,%s "%(filename,width,height))
+        for obj in root.findall('object'):
+            if obj.find('difficult')!=None:
+                difficult = obj.find('difficult').text
+                if int(difficult)==1:
+                    continue
+            obj_name = obj.find('name').text
+            bndbox = obj.find('bndbox')
+            left = bndbox.find('xmin').text
+            top = bndbox.find('ymin').text
+            right = bndbox.find('xmax').text
+            bottom = bndbox.find('ymax').text
+            new_f.write("%s,%s,%s,%s,%s " % (left, top, right, bottom,obj_name))
+        new_f.write('\n')
